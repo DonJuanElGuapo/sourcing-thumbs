1279d65982f35ff34aed40929402ac1bb0a2cbd2
DonJuanElGuapo <johnwbroadhead@gmail.com>
2026-07-15 17:41:46 -0600

    Enhance OAuth callback handler with code validation
    Updated handler to be asynchronous and added code validation.

--- api/callback.js
+++ api/callback.js
@@ -1,3 +1,12 @@
-export default function handler(req, res) {
-  res.status(200).send("OAuth Callback Received");
+export default async function handler(req, res) {
+  const { code, state } = req.query;
+
+  if (!code) {
+    return res.status(400).send("Missing code");
+  }
+
+  // TODO: Exchange code for token with eBay API
+  // Example: POST to https://api.ebay.com/identity/v1/oauth2/token
+
+  res.status(200).send(`OAuth Callback Received. Code: ${code}, State: ${state}`);
 }
