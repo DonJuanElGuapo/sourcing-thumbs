ae091687ac91ceee15d8c8293fd9b4d3a28c2dd2
DonJuanElGuapo <johnwbroadhead@gmail.com>
2026-07-16 07:54:35 -0600

    Remove OAuth handler code from callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,12 +1,5 @@
-export default async function handler(req, res) {
-  const { code, state } = req.query;
-
-  if (!code) {
-    return res.status(400).send("Missing code");
-  }
-
-  // TODO: Exchange code for token with eBay API
-  // Example: POST to https://api.ebay.com/identity/v1/oauth2/token
-
-  res.status(200).send(`OAuth Callback Received. Code: ${code}, State: ${state}`);
-}
+https://auth.ebay.com/oauth2/authorize?
+client_id=JohnBroa-Sourcing-PRD-96247139a-dbeadd8d
+&redirect_uri=John_Broadhead-JohnBroa-Sourci-ohigslv
+&response_type=code
+&scope=https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment
