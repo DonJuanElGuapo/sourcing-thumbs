0a668106cffebe1fc2dbac87f8eba6d2599705e8
DonJuanElGuapo <johnwbroadhead@gmail.com>
2026-07-16 08:58:43 -0600

    Update ebayConfig.js

--- ebayConfig.js
+++ ebayConfig.js
@@ -1,6 +1,5 @@
-// ebayConfig.js
 module.exports = {
-  APP_ID: process.env.EBAY_SANDBOX_APP_ID,
-  CERT_ID: process.env.EBAY_SANDBOX_CERT_ID,
-  DEV_ID: process.env.EBAY_SANDBOX_DEV_ID
+  APP_ID: process.env.EBAY_CLIENT_ID,
+  CERT_ID: process.env.EBAY_CLIENT_SECRET,
+  DEV_ID: process.env.EBAY_DEV_ID
 };
