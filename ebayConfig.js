cc45c9ce7442c14e036777f4a9414ac6cc76b446
DonJuanElGuapo <johnwbroadhead@gmail.com>
2026-07-16 09:07:03 -0600

    Remove DEV_ID from ebayConfig.js

--- ebayConfig.js
+++ ebayConfig.js
@@ -1,5 +1,4 @@
 module.exports = {
   APP_ID: process.env.EBAY_CLIENT_ID,
-  CERT_ID: process.env.EBAY_CLIENT_SECRET,
-  DEV_ID: process.env.EBAY_DEV_ID
+  CERT_ID: process.env.EBAY_CLIENT_SECRET
 };
